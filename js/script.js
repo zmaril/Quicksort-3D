e482ca58aea8d008527553083350a86a67403405
Zack Maril <zack@zacharymaril.com>
2012-01-01 14:18:05 -0500

    Lots of work to make it prettier and more interactive

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 // Takes in a list of objects and then uses the compare function to determine the ordering. 
 // It's expected that the comparsion functon returns -1 when a is less than b, 1 when a is greater than b, and 0 when they are equal. 
-var quicksort = function (list,compare){
-    if (list.length <= 1){
-        return list;
+Array.prototype.quicksort= function (compare){
+    //One element is already sorted.
+    if (this.length <= 1){
+        return this;
     }
-    var pivotIndex = Math.floor(list.length/2);
-    var pivot = list[pivotIndex];
+    //Simple way to pick the pivot point. 
+    var pivotIndex = Math.floor(this.length/2);
+    var pivot = this[pivotIndex];
+
+    //Arrays holding the elements that have been seperated into less and more
     var less = [];
     var more = [];
-    for(var i = 0; i <list.length; i++){
+
+    //Simple for look using compare
+    for(var i = 0; i <this.length; i++){
         if(i != pivotIndex){
-            var next = list[i];
+            var next = this[i];
             if(-1 == compare(next,pivot)){
                 less.push(next);
             }
@@ -19,7 +25,8 @@ var quicksort = function (list,compare){
             }            
         }
     }    
-    return quicksort(less,compare).concat([pivot],quicksort(more,compare));
+    //Return the array recursively sorted
+    return less.quicksort(compare).concat([pivot],more.quicksort(compare));
 };
 
 //Takes in two numbers and compares them in the standard way. 
@@ -35,24 +42,32 @@ var numCompare = function(a,b){
 };
 
 //Numberical quicksort. 
-var numQuicksort = function(list){return quicksort(list, numCompare);};
+Array.prototype.numQuicksort = function(){return this.quicksort(numCompare);};
 
-//Sort points by distance from origin. Expects the form {x: 12123, y:32121} to work with. 
-var pointSort = function (list){
-    return quicksort(list, 
-                     function(a,b){
-                         var distA= Math.sqrt(Math.pow(a.x,2) + Math.pow(a.y,2));
-                         var distB= Math.sqrt(Math.pow(b.x,2) + Math.pow(b.y,2));
-                         return numCompare(distA,distB);
-                     });
+//Takes in a list and outputs an object that contains a pivot point, all the items in the list "less" than the pivot , and all the items  in the "more" than the pivot. 
+//Example input: [3,2,4,5], numCompare
+//Example output: {less:[2],pivot:3, more:[4,5]}
+Array.prototype.quicksortStep = function (compare){
+    if (this.length <= 1){
+        return this;
+    }
+    var pivotIndex = Math.floor(this.length/2);
+    var pivot = this[pivotIndex];
+    var less = [];
+    var more = [];
+    for(var i = 0; i <this.length; i++){
+        if(i != pivotIndex){
+            var next = this[i];
+            if(-1 == compare(next,pivot)){
+                less.push(next);
+            }
+            else {
+                more.push(next);
+            }            
+        }
+    }    
+    return {less:less, pivot:pivot, more: more};
 };
-
-
-/*
- * Animating everything happens below here.
- * 
- * 
- */
 
 /**
  * Provides requestAnimationFrame in a cross browser way.
@@ -60,7 +75,6 @@ var pointSort = function (list){
  */
 
 if ( !window.requestAnimationFrame ) {
-
 	window.requestAnimationFrame = ( function() {
 
 		return window.webkitRequestAnimationFrame ||
@@ -74,12 +88,12 @@ if ( !window.requestAnimationFrame ) {
 		};
 
 	} )();
-
 }
 
 
-//Copied and pasted from 
+//Displaying everything
 //https://github.com/mrdoob/three.js/blob/master/examples/canvas_particles_random.html
+//http://mrdoob.com/projects/voxels/#A/afhaddadkShahhShaffShSh
 //Modified to get the quicksort out there correctly. 
 var container, stats;
 var camera, scene, renderer, group, particle;
@@ -88,21 +102,80 @@ var mouseX = 0, mouseY = 0;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
+var projector,ray, objectHovered,
+    mouse3D, isMouseDown = false, onMouseDownPosition,
+    radious = 5000, theta = 45, onMouseDownTheta = 45, phi = 60, onMouseDownPhi = 60,
+    isShiftDown = false;
+var buttons; 
+var speed = 1000;
+
+//The particles that need to be quicksorted away 
+var leftovers;
+
+setup();
 init();
 animate();
 
-function init() {
+function setup(){
+    //Setting up the controls and container
+    buttons = $( "#radio" ).buttonset()
+        .click(function(event){
+        var target = $(event.target);
+        switch(target.attr('id')){
+            case("speed1"):
+            speed = 3000;
+            break;
+
+            case("speed2"):
+            speed = 1000; 
+            break;
+
+            case("speed3"):
+            speed = 333;
+            break;
+            
+            default: break;
+        }
+
+        })
+        .parent()
+        .offset({top:10, left:100});
 
     container = document.createElement( 'div' );
-    
+
     document.body.appendChild( container );
 
-    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.x = 3000;    
-    camera.position.z = 3000;    
-
+    //Getting the camera ready to be moved correctly.
+    camera = new THREE.Camera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.x = radious * Math.sin( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
+    camera.position.y = radious * Math.sin( phi * Math.PI / 360 );
+    camera.position.z = radious * Math.cos( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
 
     scene = new THREE.Scene();
+    //http://nooshu.com/debug-axes-in-three-js
+    //Adding in an axis
+    var v = function (x,y,z){ 
+            return new THREE.Vertex(new THREE.Vector3(x,y,z)); 
+    };
+    
+    //Create axis (point1, point2, colour)
+    var createAxis = function (p1, p2, color){
+            var line, lineGeometry = new THREE.Geometry(),
+            lineMat = new THREE.LineBasicMaterial({color: color, lineWidth: 1});
+            lineGeometry.vertices.push(p1, p2);
+            line = new THREE.Line(lineGeometry, lineMat);
+            scene.add(line);
+    };       
+
+    var axisLength= 3000;
+    createAxis(v(0, 0, -axisLength), v(0, 0, axisLength), 0x0000FF);
+    group = new THREE.Object3D();
+    scene.add( group );
+}
+
+
+
+function init() {
 
     var PI2 = Math.PI * 2;
     var program = function ( context ) {
@@ -110,17 +183,11 @@ function init() {
 	context.arc( 0, 0, 1, 0, PI2, true );
 	context.closePath();
 	context.fill();
-    };
-
-    group = new THREE.Object3D();
-    scene.add( group );
-    //300300910 red
-    // 0 black
-    // 1000 blue
-    for ( var i = 0; i < 100; i++ ) {
-	particle = new THREE.Particle( new THREE.ParticleCanvasMaterial( { color: 0
-                                                                           //Math.random() * 0x808008 + 0x808080
-                                                                           , program: program } ) );
+    };        
+    
+    //Creating a group to hold all the particles
+    for ( var i = 0; i < 600; i++ ) {
+	particle = new THREE.Particle( new THREE.ParticleCanvasMaterial( { color: 0, program: program } ) );
 	particle.position.x = Math.random() * 2000 - 1000;
 	particle.position.y = Math.random() * 2000 - 1000;
 	particle.position.z = Math.random() * 2000 - 1000;
@@ -128,111 +195,75 @@ function init() {
 	group.add( particle );
     }
 
+    //Three js stuff
     renderer = new THREE.CanvasRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     container.appendChild( renderer.domElement );
+    onMouseDownPosition = new THREE.Vector2();
 
-    stats = new Stats();
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.top = '0px';
-    container.appendChild( stats.domElement );
-
+    //Set up event controls
     document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.addEventListener( 'touchstart', onDocumentTouchStart, false );
-    document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+    document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+    document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+    leftovers =[group.children]; 
 }
 
-//
 
+//Capture the mouse down positon
+function onDocumentMouseDown( event ) {
+    event.preventDefault();
+
+    isMouseDown = true;
+
+    onMouseDownTheta = theta;
+    onMouseDownPhi = phi;
+    onMouseDownPosition.x = event.clientX;
+    onMouseDownPosition.y = event.clientY;
+}
+
+//Track information and update view
 function onDocumentMouseMove( event ) {
+    event.preventDefault();
 
-    mouseX = event.clientX - windowHalfX;
-    mouseY = event.clientY - windowHalfY;
-}
+    if ( isMouseDown ) {
+        
+	theta = - ( ( event.clientX - onMouseDownPosition.x ) * 0.5 ) + onMouseDownTheta;
+	phi = ( ( event.clientY - onMouseDownPosition.y ) * 0.5 ) + onMouseDownPhi;
+        
+	phi = Math.min( 180, Math.max( 0, phi ) );
 
-function onDocumentTouchStart( event ) {
+	camera.position.x = radious * Math.sin( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
+	camera.position.y = radious * Math.sin( phi * Math.PI / 360 );
+	camera.position.z = radious * Math.cos( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
+	camera.updateMatrix();
 
-    if ( event.touches.length == 1 ) {
-
-	event.preventDefault();
-
-	mouseX = event.touches[ 0 ].pageX - windowHalfX;
-	mouseY = event.touches[ 0 ].pageY - windowHalfY;
     }
+    render();
+
 }
 
-function onDocumentTouchMove( event ) {
-
-    if ( event.touches.length == 1 ) {
-
-	event.preventDefault();
-
-	mouseX = event.touches[ 0 ].pageX - windowHalfX;
-	mouseY = event.touches[ 0 ].pageY - windowHalfY;
-    }
+//Stop tracking info
+function onDocumentMouseUp( event ) {
+    event.preventDefault();
+    isMouseDown = false;
 }
 
-//
 
 function animate() {
     requestAnimationFrame( animate );
     render();
-    stats.update();
 }
-
-
 
 function render() {
-
-    camera.position.x += ( mouseX - camera.position.x )*0.05;;
-    camera.position.y += ( - mouseY - camera.position.y )*0.05;
     camera.lookAt( scene.position );
-
-    group.rotation.x += 0.01;
     renderer.render( scene, camera );
-
 }
 
 
-//Takes in a list and outputs an object that contains a pivot point, all the items in the list "less" than the pivot , and all the items  in the "more" than the pivot. 
-//Example input: [3,2,4,5], numCompare
-//Example output: {less:[2],pivot:3, more:[4,5]}
 
-var quicksortStep = function (list,compare){
-    if (list.length <= 1){
-        return list;
-    }
-    var pivotIndex = Math.floor(list.length/2);
-    var pivot = list[pivotIndex];
-    var less = [];
-    var more = [];
-    for(var i = 0; i <list.length; i++){
-        if(i != pivotIndex){
-            var next = list[i];
-            if(-1 == compare(next,pivot)){
-                less.push(next);
-            }
-            else {
-                more.push(next);
-            }            
-        }
-    }    
-    return {less:less, pivot:pivot, more: more};
-};
-
-
-var leftovers =[group.children]; 
-
-var pickPivot = function(){
-    var pivot = kids[Math.floor(Math.random()*kids.length)];
-
-    messyState.pivot= pivot;
-    messyState.turn=1;
-};
-
-var id = setInterval(function(){
+//The function that loops through the leftovers and sets up the timing for animation. 
+var changeFunction = function(){
     if(leftovers.length == 0){
-        clearInterval(id);
         return;
     }
 
@@ -242,18 +273,20 @@ var id = setInterval(function(){
         return;
     }
 
-    var quicksorted= quicksortStep(next,function(a,b){
-                         var distA= Math.sqrt(Math.pow(a.position.x,2) + Math.pow(a.position.y,2));
-                         var distB= Math.sqrt(Math.pow(b.position.x,2) + Math.pow(b.position.y,2));
+    var quicksorted= next.quicksortStep(function(a,b){
+                         var distA= Math.pow(a.position.x,2) + Math.pow(a.position.y,2);
+                         var distB= Math.pow(b.position.x,2) + Math.pow(b.position.y,2);
                          return numCompare(distA,distB);
                      });
     
     var listOfZ =[];
+
     next.forEach(function(point){
         listOfZ.push(point.position.z);
+        point.material.color.setRGB(0,0,0);        
         });
 
-    var sortedZ= numQuicksort(listOfZ);
+    var sortedZ= listOfZ.numQuicksort();
     var lessZ= sortedZ.slice(0,quicksorted.less.length);
     var pivotZ= sortedZ.slice(quicksorted.less.length,quicksorted.less.length+1);
     var moreZ= sortedZ.slice(quicksorted.less.length+1);
@@ -273,9 +306,9 @@ var id = setInterval(function(){
         });
     };
 
-    setTimeout(changePivotColor.bind(quicksorted),200);    
-    setTimeout(changeMoreColor.bind(quicksorted),400);
-    setTimeout(changeLessColor.bind(quicksorted),600);
+    setTimeout(changePivotColor.bind(quicksorted),speed*0.2);    
+    setTimeout(changeMoreColor.bind(quicksorted),speed*0.4);
+    setTimeout(changeLessColor.bind(quicksorted),speed*0.6);
 
     var moveLess = function(){
         quicksorted.less.forEach(function(point){
@@ -293,16 +326,33 @@ var id = setInterval(function(){
             });
     };
 
-    setTimeout(moveLess.bind(quicksorted,lessZ),700);
-    setTimeout(movePivot.bind(quicksorted,pivotZ),800);
-    setTimeout(moveMore.bind(quicksorted,moreZ),700);
+    var clear = function(){
+        group.children.forEach(function(point){
+            point.material.color.setRGB(0,0,0);
+            });
+        };
 
-
-    if (quicksorted.more.length > 1){
-        leftovers.push(quicksorted.more);
+    setTimeout(moveLess.bind(quicksorted,lessZ),0.7*speed);
+    setTimeout(movePivot.bind(quicksorted,pivotZ),0.8*speed);
+    setTimeout(moveMore.bind(quicksorted,moreZ),0.9*speed);
+    setTimeout(clear,950);
+    
+    
+    if (quicksorted.more.length > 0){
+        var pivotAndMore = [quicksorted.pivot].concat(quicksorted.more);
+        leftovers.push(pivotAndMore);
     }
     if (quicksorted.less.length > 1){
         leftovers.push(quicksorted.less);
     }
 
-    },1000);
+    //Sort the arrays with quicksort. It looks better to do all the big stuff first.
+    leftovers = leftovers.quicksort(function(a,b){
+        return numCompare(a.length,b.length);
+    });
+
+    setTimeout(changeFunction,speed);
+};
+
+setTimeout(changeFunction,speed);
+
